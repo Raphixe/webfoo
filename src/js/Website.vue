ac3c1044c61f0f21b1049dff7e93de8abb72c98a
Kevin Ruscoe <kevin@popcreative.co.uk>
2018-11-29 20:28:14 +0000

    add shite

---
+++ src/js/Website.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            scrollY: 0
+        }
+    },
+    computed: {
+        shouldBackgroundNav() {
+            return this.scrollY > 50;
+        }
+    },
+    mounted() {
+        window.addEventListener('scroll', (e) => {
+            this.scrollY = window.scrollY;
+        });
+    }
+}
+</script>
